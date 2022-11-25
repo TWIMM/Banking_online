1f9eea0c53f3b2b2783a77e38cb9eb40eeaf4222
TWIMM <twimm@gmail.com>
2022-11-25 08:17:25 +0100

    technical fixex

--- Components/Opérations.js
+++ Components/Opérations.js
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text  , TouchableOpacity} from 'react-native';
+import React from 'react';
+import styles from './styles';
+
 
 export default function Opérations() {
   return (
-    <View>
-      <Text>Opérations</Text>
+    <View style={styles.operationscontainer}>
+          
+          <TouchableOpacity style={styles.operationscontainer.childone}>
+              <Text style={[styles.username , {color:'black'} ]}>
+                    Faire un transfert
+              </Text >
+          </TouchableOpacity >
+
+          <TouchableOpacity style={styles.operationscontainer.childthree}>
+              <Text style={[styles.username , {color:'black'} ]}>
+                  Demander une carte
+              </Text >
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.operationscontainer.childthree}>
+              <Text style={[styles.username , {color:'black'} ]}>
+                   Confirmer un achat en ligne
+              </Text >
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.operationscontainer.childthree}>
+              <Text style={[styles.username , {color:'black'} ]}>
+                 Retrait par POS
+              </Text >
+          </TouchableOpacity>
+
     </View>
   )
 }
--- Components/Transactions.js
+++ Components/Transactions.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text  , TouchableOpacity} from 'react-native';
+import React from 'react';
+import styles from './styles';
 
 export default function Transactions() {
   return (
-    <View>
-      <Text>Transactions</Text>
-    </View>
+    <View style={styles.transaccontainer}>
+          
+    <View style={styles.transaccontainer}>
+        <Text style={[styles.username , {color:'black' , fontSize:'20%'} ]}>
+              Aucune transaction pour ce mois.
+        </Text >
+    </View >
+
+
+</View>
   )
 }
